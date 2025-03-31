dcdd2b2611b31878204a1c54b78a9fa29b4c575f
Sandra_Morkuniene <97901212+SandraMorkuniene@users.noreply.github.com>
2025-03-31 22:24:23 +0300

    Update AI_chatbot_app.py

--- Deployment/AI_chatbot_app.py
+++ Deployment/AI_chatbot_app.py
@@ -97,7 +97,7 @@ for message in st.session_state.conversation_history:
     st.chat_message(message["role"]).markdown(message["content"])
 
 if st.session_state.model_confirmed:
-    # Use st.chat_input() instead of st.text_input()
+    
     query = st.chat_input("Ask a question:")
 
     if query:
